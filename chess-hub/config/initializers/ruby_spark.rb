5f80fe41b3f9bc57abbfc64496fadcabad9af2e5
Eli Fatsi <eli.fatsi@viget.com>
2022-02-05 08:11:57 -0500

    Store access token in rails credentials

--- chess-hub/config/initializers/ruby_spark.rb
+++ chess-hub/config/initializers/ruby_spark.rb
@@ -1,4 +1,4 @@
 RubySpark.configuration do |config|
-  config.access_token = "02a6a7e1353017b2840b1a8685a58eb004c81319"
+  config.access_token = Rails.application.credentials.particle_access_token
   config.timeout      = 5.seconds
 end
