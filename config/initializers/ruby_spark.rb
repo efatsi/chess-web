bf1aa58ab89ff7c38300eee0704c0372f43747b0
Eli Fatsi <eli.fatsi@viget.com>
2022-07-04 20:56:19 -0400

    Lean on heroku config for particle token

--- config/initializers/ruby_spark.rb
+++ config/initializers/ruby_spark.rb
@@ -1,4 +1,4 @@
 RubySpark.configuration do |config|
-  config.access_token = Rails.application.credentials.particle_access_token
+  config.access_token = ENV["PARTICLE_TOKEN"] || Rails.application.credentials.particle_access_token
   config.timeout      = 5.seconds
 end
